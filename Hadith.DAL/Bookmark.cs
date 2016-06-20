d1c863400e03e39e56cd4d8824187436beeb59e2
mifmasterz <mifmasterz@yahoo.com>
2016-06-20 16:43:19 +0700

    compiled
    don;t forget to extract database in debug folder

---
+++ Hadith.DAL/Bookmark.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hadith.DAL
+{
+    [Table(Name = "Bookmark")]
+    public class Bookmark
+    {
+        [Column(Name = "idx")]
+        public int idx { get; set; }
+
+        [Column(Name = "Title")]
+        public string Title { get; set; }
+
+        [Column(Name = "HadithId")]
+        public int HadithId { get; set; }
+
+        [Column(Name = "PageNo")]
+        public int PageNo { get; set; }
+
+        [Column(Name = "ChapterNo")]
+        public int ChapterNo { get; set; }
+
+        [Column(Name = "HadithNo")]
+        public int HadithNo { get; set; }
+
+
+    }
+}
